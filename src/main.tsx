ae1f928b02fbbde9ea81b3c33ffd1e102360cbce
pengjun <peng.jun@foxmail.com>
2022-02-22 18:01:28 +0800

    状态管理

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import Routes from './router/routes'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
+
+import App from './app'
 
 import './assets/css/base.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
