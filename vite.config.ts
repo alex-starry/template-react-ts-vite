b7d26807f7811c48ed8c2035fac4a7176e01ea43
pengjun <peng.jun@foxmail.com>
2022-02-22 11:23:48 +0800

    环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default ({ mode, command }) => {
+  const config: UserConfig = {
+    plugins: [react()]
+  }
+  if (command === 'serve') {
+    config.server = {
+      proxy: {
+        '/mockGateway': {
+          // target: 'https://qd2-gw.dev.qdingnet.com',
+          target: 'https://qd2-gw.qa.qdingnet.com',
+          // target: 'https://qd2-gw.qdingnet.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/mockGateway/, '')
+        }
+      }
+    }
+  }
+  return config
+}
